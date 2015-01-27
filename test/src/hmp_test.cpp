de712c562688c0c450180a423c1693538c76922d
Ye Tao <taoyeandy@Andy-Tao.local>
2015-01-27 17:12:42 +0100

    double checked with matlab code, added with resize image function. now it is the same as what HMP matlab code does

--- test/src/hmp_test.cpp
+++ test/src/hmp_test.cpp
@@ -3,6 +3,7 @@
 #include "hmp.h"
 #include <opencv2/opencv.hpp>
 #include <Eigen/Dense>
+#include <opencv2/core/eigen.hpp>
 #include <Eigen/Core>
 #include <fstream>
 #include <chrono>
@@ -17,43 +18,31 @@ using namespace std::chrono;
 
 int main(){
 
-  /** rgbd */
-  HMP hmp("hmp.config", "rgbd","second+first");
-  char rgbdir[] = "apple_1_1_1_crop.png";
-  char depthdir[] = "apple_1_1_1_depthcrop.png";
+      HMP hmp("hmp.config", "rgb","second+first");
+      char rgbdir[] = "bell_pepper_3_1_1_crop.png";
+      char depthdir[] = "bell_pepper_3_1_1_depthcrop.png";
 
-  VectorXd fea;
-  high_resolution_clock::time_point t1 = high_resolution_clock::now();
-  hmp.computeHMP(rgbdir, depthdir, fea);
-  high_resolution_clock::time_point t2 = high_resolution_clock::now();
+      VectorXd fea;
+      high_resolution_clock::time_point t1 = high_resolution_clock::now();
+      hmp.computeHMP(rgbdir, fea);
+      high_resolution_clock::time_point t2 = high_resolution_clock::now();
 
-  cout << "Time spent for HMP feature: "
-       << duration_cast<milliseconds>(t2-t1).count()<<endl;
+      cout << "Time spent for HMP feature: "
+	       << duration_cast<milliseconds>(t2-t1).count()<<endl;
 
-  /** rgb */
-  // HMP hmp("hmp.config", "rgb","second+first");
-  // char rgbdir[] = "apple_1_1_1_crop.png";
+			
+      // Mat X;
+      // X = cvCreateMat(100,100,CV_8UC3);
+      // randu(X, Scalar::all(0), Scalar::all(0));
+      // cout << X << endl << endl;
 
-  // VectorXd fea;
-  // high_resolution_clock::time_point t1 = high_resolution_clock::now();
-  // hmp.computeHMP(rgbdir, fea);
-  // high_resolution_clock::time_point t2 = high_resolution_clock::now();
+      // cout << X-1 << endl;
+      // imshow("window",X);
+      // waitKey(0);
+      ofstream ofile("patchMat.dat");
+      ofile << fea << endl;
+      ofile.close();
 
-  // cout << "Time spent for HMP feature: "
-  // 	       << duration_cast<milliseconds>(t2-t1).count()<<endl;
-
-  /** depth */
       
-  // HMP hmp("hmp.config", "depth","second+first");
-  // char depthdir[] = "apple_1_1_1_depthcrop.png";
-
-  // VectorXd fea;
-  // high_resolution_clock::time_point t1 = high_resolution_clock::now();
-  // hmp.computeHMP(depthdir, fea);
-  // high_resolution_clock::time_point t2 = high_resolution_clock::now();
-
-  // cout << "Time spent for HMP feature: "
-  // 	   << duration_cast<milliseconds>(t2-t1).count()<<endl;
-      
-  return 0;
+      return 0;
 }
